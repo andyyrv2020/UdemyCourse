5e7c0b9c8aabfb1f9ece4de9221b5a1ee3da4ed2
Андрей Рашев <ar42698937@edu.mon.bg>
2024-01-10 19:35:18 +0200

    If/Else conditions added, and Methods#1

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello!");
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("[S]ee all TODOs");
+            Console.WriteLine("[A]dd TODO");
+            Console.WriteLine("[R]emove TODO");
+            Console.WriteLine("[E]xit");
+
+            var userChoise = Console.ReadLine();
+
+            if (userChoise == "S")
+            {
+                PrintSelectedOption("See all TODOs");
+            }
+            else if (userChoise == "A")
+            {
+                PrintSelectedOption("Add TODO");
+            }
+            else if (userChoise == "R")
+            {
+                PrintSelectedOption("Remove TODO");
+            }
+            else if (userChoise == "E")
+            {
+                PrintSelectedOption("Exit");
+            }
+
+            void PrintSelectedOption(string selectedOption)
+            {
+                Console.WriteLine("Selected option: " + selectedOption);
+            }
         }
     }
 }
