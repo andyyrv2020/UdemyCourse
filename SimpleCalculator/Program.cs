19b2884ef862421e7f1f5bebff5233a85b780757
PC-23 <it.pgee.burgas@gmail.com>
2024-01-16 13:42:05 +0200

    Added variables

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleCalculator
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		double firstInput = double.Parse(Console.ReadLine());
+		double secondInput = double.Parse(Console.ReadLine());
+		}
+	}
+}
